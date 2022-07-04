b91875f655308471e7bfbe47a9d688d859b8e8d6
moon <xmun74@naver.com>
2022-07-04 22:14:34 +0900

    #1.1 ~ #1.2 Store and Reducer, Actions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
+//createStore -  바뀌는 state 데이터를 넣는 곳
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// reducer함수 : data를 변경하는 함수 (초기값 0)
+// action: 두번째 인자
+const countModifier = (count = 0, action) => {
+  // console.log(action); // { type: "안녕여어" }
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const handleAdd = () => {
-  count++;
-  updateText(); // 리페인팅 - 값 업데이트
-};
+// createStore(reducer를 요구함) : data를 저장하는 곳
+const countStore = createStore(countModifier);
+// console.log(countStore.getState()); // 0
 
-const handleMinus = () => {
-  count--;
-  updateText(); // 리페인팅 - 값 업데이트
-};
+// dispatch : action 전달
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
