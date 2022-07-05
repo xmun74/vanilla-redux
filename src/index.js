8bebdc67fd3a4254a514cbcbc5fa4b885d2ea1eb
moon <xmun74@naver.com>
2022-07-05 16:30:22 +0900

    #1.4 Recap, Refator

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 number.innerText = 0;
 
-// reducer 함수 : data를 변경하는 함수 (초기값 0)
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// reducer 함수 : data를 변경하는 함수 (초기값 0) , dispatch에 action 전달
+// reducer의 리턴 : application state가 됨
 // action 객체 : reducer 함수의 두번째 인자 {type: 'MINUS'}
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -28,12 +33,12 @@ const onChange = () => {
 countStore.subscribe(onChange);
 // subscribe - store안에 변화를 알 수 있게 해준다
 
-// dispatch : action 전달
+// dispatch : action 받아옴, type 프로퍼티로 전달해야함
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
